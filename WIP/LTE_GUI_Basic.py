5ac0e554b424eaada75231534afa0efd042220a6
Brian Han <bmhan@ucsd.edu>
2017-07-24 18:40:38 -0700

    Added update to refresh window

--- WIP/LTE_GUI_Basic.py
+++ WIP/LTE_GUI_Basic.py
@@ -7,18 +7,31 @@
 # -----------------------------------------------------------------------------
 from Tkinter import *
 import test_miniUT_crystal_rb as tester
+RESULT = False
 
-class Example3:
+
+class LTE_Test:
     def __init__ (self, master):
-        self.lbl = Label(master, text="Press the button below to run the test!")
-        self.lbl.pack()
-        self.btn = Button(master, text="Run Test", command=self.test)
-        self.btn.pack()
-
+        self.master = master
+        Title = Label(master, text="LTE Test")
+        Title.grid(row = 2, column = 1)
+        testBtn = Button(master, text="Run Test", command=self.test)
+        testBtn.grid(row = 4, column = 1)
+        testRes = Label (master, text = "", fg = 'blue')
+        testRes.grid(row = 6, column = 1)
+        
+        
     #Now takes in a parameter
     def test(self):
-        tester.main()
-
+        testRes = Label (self.master, text = "Test is now running", fg = 'blue')
+        testRes.grid(row = 6, column = 1)
+        self.master.update() 
+        RESULT = tester.main()
+        if RESULT == True:
+            testRes.config (text = "Test passed", fg = 'green', fb = 'white')
+        else:
+            testRes.config(text = "Test failed", fg = 'red', fb = 'white')
+        
 root = Tk( )
-ex3 = Example3(root)
+lte = LTE_Test(root)
 root.mainloop( )
